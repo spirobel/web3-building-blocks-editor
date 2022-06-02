560ee0527eb25ac518e50d4b37cd38819f7a164e
spirobel <spirobel@protonmail.com>
2022-06-02 12:04:15 +0800

    build into the discourse plugin folder adjacent to it

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: '../web3-building-blocks/public/editor',
+    emptyOutDir: true
+  }
 })
