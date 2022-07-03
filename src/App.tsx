751d9070b8ec29e6293c245fe58bdbfd6099c9ac
spirobel <spirobel@protonmail.com>
2022-07-03 21:03:49 +0800

    not logged in and installed react-location  routing lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  function handleClick() {
-    console.log('click')
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={handleClick}>
-            count is:
-          </button>
-        </p>
-
-        <div>
-          <p>Dogs to fetch:</p>
-        </div>
-
-        <div />
-
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return <div>not logged in</div>
 }
 
 export default App
