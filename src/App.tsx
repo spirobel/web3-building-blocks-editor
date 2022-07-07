f794a40945d8ef5dfa432ea27a2c10a06ced4b43
spirobel <spirobel@protonmail.com>
2022-07-07 17:15:42 +0800

    add first draft of Login.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { Outlet, ReactLocation, Router } from '@tanstack/react-location'
+import {
+  Outlet,
+  ReactLocation,
+  Router,
+  Navigate,
+} from '@tanstack/react-location'
+import 'antd/dist/antd.css'
 import './App.css'
+import Login from './editor-ui/Login'
 
 const location = new ReactLocation()
 function App() {
+  // the components that need a menu should be wrapped in the MenuWrapper.tsx component
   return (
     <Router
       location={location}
-      routes={[{ path: '/login', element: <div>not logged in</div> }]}
+      routes={[{ path: '/login', element: <Login /> }]}
     >
       <Outlet />
+      <Navigate to="./login" />
     </Router>
   )
 }
---
+++ src/editor-ui/Login.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Button, Form, Input } from 'antd'
+
+export default function Login() {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  return (
+    <div className="grid place-items-center h-screen ">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
