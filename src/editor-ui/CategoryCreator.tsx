8e61ea7851e8de52b705cbcda4f0717a3c662dc7
spirobel <spirobel@protonmail.com>
2022-07-22 23:05:19 +0800

    category name and description input fields

--- src/editor-ui/CategoryCreator.tsx
+++ src/editor-ui/CategoryCreator.tsx
@@ -1,64 +1,25 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Input } from 'antd'
 import { MenuWrapper } from './MenuWrapper'
+
+const { TextArea } = Input
 
 export function CategoryCreator() {
   return (
     <MenuWrapper>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
+          title={<Input placeholder="category name" />}
+          // extra={<a href="#">More</a>}
           style={{ width: 300 }}
         >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
-          style={{ width: 300 }}
-        >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
-          style={{ width: 300 }}
-        >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
-          style={{ width: 300 }}
-        >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
-          style={{ width: 300 }}
-        >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card
-          title="Default size card"
-          extra={<a href="#">More</a>}
-          style={{ width: 300 }}
-        >
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+          <TextArea
+            autoSize={{ minRows: 4, maxRows: 4 }}
+            showCount
+            maxLength={116}
+            //  style={{ height: 100 }}
+            placeholder="description"
+          />
         </Card>
       </div>
     </MenuWrapper>
