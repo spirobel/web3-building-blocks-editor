07c4b01de7da219f994cc71c08a2cc9a15bc22df
spirobel <spirobel@protonmail.com>
2022-07-22 12:14:50 +0800

    categories grid responsiveness cols

--- src/editor-ui/CategoryCreator.tsx
+++ src/editor-ui/CategoryCreator.tsx
@@ -1,10 +1,66 @@
 import React from 'react'
+import { Card } from 'antd'
 import { MenuWrapper } from './MenuWrapper'
 
 export function CategoryCreator() {
   return (
     <MenuWrapper>
-      <div>hello from CategoryCreator</div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card
+          title="Default size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </div>
     </MenuWrapper>
   )
 }
