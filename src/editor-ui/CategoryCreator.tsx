c932db6a350f5f0f6c388021990b4d408df0c988
spirobel <spirobel@protonmail.com>
2022-07-25 17:06:16 +0800

    change category permissions modal

--- src/editor-ui/CategoryCreator.tsx
+++ src/editor-ui/CategoryCreator.tsx
@@ -1,40 +1,69 @@
-import React from 'react'
-import { Card, Form, Input, Button, Row, Tag } from 'antd'
+import React, { useState } from 'react'
+import { Card, Form, Input, Button, Row, Tag, Modal } from 'antd'
 import { MenuWrapper } from './MenuWrapper'
 
 const { TextArea } = Input
 
 export function CategoryCreator() {
-  return (
-    <MenuWrapper>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <Form>
-          <Card
-            title={<Input placeholder="category name" />}
-            style={{ width: 300 }}
-          >
-            <TextArea
-              autoSize={{ minRows: 4, maxRows: 4 }}
-              showCount
-              maxLength={116}
-              placeholder="description"
-            />
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
-            <div className="mt-8">
-              <Tag style={{ float: 'left' }} color="green">
-                public category
-              </Tag>
-              <Form.Item style={{ marginBottom: 'unset' }}>
-                <Row justify="end">
-                  <Button type="primary" htmlType="submit">
-                    create!
-                  </Button>
-                </Row>
-              </Form.Item>
-            </div>
-          </Card>
-        </Form>
-      </div>
-    </MenuWrapper>
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  return (
+    <>
+      <Modal
+        title="Switch category permissions"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <MenuWrapper>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <Form>
+            <Card
+              title={<Input placeholder="category name" />}
+              style={{ width: 300 }}
+            >
+              <TextArea
+                autoSize={{ minRows: 4, maxRows: 4 }}
+                showCount
+                maxLength={116}
+                placeholder="description"
+              />
+
+              <div className="mt-8">
+                <Tag
+                  style={{ float: 'left', cursor: 'pointer' }}
+                  onClick={showModal}
+                  color="green"
+                >
+                  public category
+                </Tag>
+                <Form.Item style={{ marginBottom: 'unset' }}>
+                  <Row justify="end">
+                    <Button type="primary" htmlType="submit">
+                      create!
+                    </Button>
+                  </Row>
+                </Form.Item>
+              </div>
+            </Card>
+          </Form>
+        </div>
+      </MenuWrapper>
+    </>
   )
 }
