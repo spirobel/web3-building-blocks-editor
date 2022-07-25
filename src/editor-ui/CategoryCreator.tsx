1970d6552a044b3c6cf205aff56c925550e8bd09
spirobel <spirobel@protonmail.com>
2022-07-25 16:52:48 +0800

    add form and public/ private category tag

--- src/editor-ui/CategoryCreator.tsx
+++ src/editor-ui/CategoryCreator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Input } from 'antd'
+import { Card, Form, Input, Button, Row, Tag } from 'antd'
 import { MenuWrapper } from './MenuWrapper'
 
 const { TextArea } = Input
@@ -8,17 +8,32 @@ export function CategoryCreator() {
   return (
     <MenuWrapper>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <Card
-          title={<Input placeholder="category name" />}
-          style={{ width: 300 }}
-        >
-          <TextArea
-            autoSize={{ minRows: 4, maxRows: 4 }}
-            showCount
-            maxLength={116}
-            placeholder="description"
-          />
-        </Card>
+        <Form>
+          <Card
+            title={<Input placeholder="category name" />}
+            style={{ width: 300 }}
+          >
+            <TextArea
+              autoSize={{ minRows: 4, maxRows: 4 }}
+              showCount
+              maxLength={116}
+              placeholder="description"
+            />
+
+            <div className="mt-8">
+              <Tag style={{ float: 'left' }} color="green">
+                public category
+              </Tag>
+              <Form.Item style={{ marginBottom: 'unset' }}>
+                <Row justify="end">
+                  <Button type="primary" htmlType="submit">
+                    create!
+                  </Button>
+                </Row>
+              </Form.Item>
+            </div>
+          </Card>
+        </Form>
       </div>
     </MenuWrapper>
   )
